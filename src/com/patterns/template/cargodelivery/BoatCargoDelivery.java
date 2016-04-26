13a8f0b83a143c0225ba967a0dd08d950a2cd838
Cedric Kim A. Sarmenta <cedric@Cedrics-MacBook-Pro.local>
2016-04-26 23:52:28 +0800

    Temporarily remove projects

--- src/com/patterns/template/cargodelivery/BoatCargoDelivery.java
+++ src/com/patterns/template/cargodelivery/BoatCargoDelivery.java
@@ -1,14 +1,14 @@
-package com.patterns.template.cargodelivery;
-
-public class BoatCargoDelivery extends CargoDelivery{
-//	@Override
-//	protected void transport(){
-//		System.out.println("Delivering cargo through the angry sea.");
-//	}
-//	
-//	@Override
-//	protected String getVehicleType(){
-//		return "Boat";
-//	}
-
-}
+//package com.patterns.template.cargodelivery;
+//
+//public class BoatCargoDelivery extends CargoDelivery{
+////	@Override
+////	protected void transport(){
+////		System.out.println("Delivering cargo through the angry sea.");
+////	}
+////	
+////	@Override
+////	protected String getVehicleType(){
+////		return "Boat";
+////	}
+//
+//}
