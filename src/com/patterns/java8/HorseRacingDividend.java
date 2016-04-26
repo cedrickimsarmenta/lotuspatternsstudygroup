3b70d4dce2c186766a660179eca0f86c74ae66af
Cedric Kim A. Sarmenta <cedric@Cedrics-MacBook-Pro.local>
2016-04-27 00:20:43 +0800

    Added comment

---
+++ src/com/patterns/java8/HorseRacingDividend.java
@@ -0,0 +1,46 @@
+package com.patterns.java8;
+
+public class HorseRacingDividend {
+	private String outcomeFeedCode1;
+	private String outcomeFeedCode2;
+	private String outcomeFeedCode3;
+	private String outcomeFeedCode4;
+	private String outcomeFeedCode5;
+	
+	public String getOutcomeFeedCode1() {
+		return outcomeFeedCode1;
+	}
+	public void setOutcomeFeedCode1(String outcomeFeedCode1) {
+		this.outcomeFeedCode1 = outcomeFeedCode1;
+	}
+	public String getOutcomeFeedCode2() {
+		return outcomeFeedCode2;
+	}
+	public void setOutcomeFeedCode2(String outcomeFeedCode2) {
+		this.outcomeFeedCode2 = outcomeFeedCode2;
+	}
+	public String getOutcomeFeedCode3() {
+		return outcomeFeedCode3;
+	}
+	public void setOutcomeFeedCode3(String outcomeFeedCode3) {
+		this.outcomeFeedCode3 = outcomeFeedCode3;
+	}
+	public String getOutcomeFeedCode4() {
+		return outcomeFeedCode4;
+	}
+	public void setOutcomeFeedCode4(String outcomeFeedCode4) {
+		this.outcomeFeedCode4 = outcomeFeedCode4;
+	}
+
+	public void setOutcomeFeedCode5(String outcomeFeedCode5) {
+		this.outcomeFeedCode5 = outcomeFeedCode5;
+	}
+	@Override
+	public String toString() {
+		return "HorseRacingDividend [outcomeFeedCode1=" + outcomeFeedCode1 + ", outcomeFeedCode2=" + outcomeFeedCode2
+				+ ", outcomeFeedCode3=" + outcomeFeedCode3 + ", outcomeFeedCode4=" + outcomeFeedCode4
+				+ ", outcomeFeedCode5=" + outcomeFeedCode5 + "]";
+	}
+	
+	
+}
--- src/com/patterns/java8/MethodReflection.java
+++ src/com/patterns/java8/MethodReflection.java
@@ -5,61 +5,34 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MethodReflection {
-	private static class HorseRacingDividend {
-		private String outcomeFeedCode1;
-		private String outcomeFeedCode2;
-		private String outcomeFeedCode3;
-		private String outcomeFeedCode4;
-		public String getOutcomeFeedCode1() {
-			return outcomeFeedCode1;
-		}
-		public void setOutcomeFeedCode1(String outcomeFeedCode1) {
-			this.outcomeFeedCode1 = outcomeFeedCode1;
-		}
-		public String getOutcomeFeedCode2() {
-			return outcomeFeedCode2;
-		}
-		public void setOutcomeFeedCode2(String outcomeFeedCode2) {
-			this.outcomeFeedCode2 = outcomeFeedCode2;
-		}
-		public String getOutcomeFeedCode3() {
-			return outcomeFeedCode3;
-		}
-		public void setOutcomeFeedCode3(String outcomeFeedCode3) {
-			this.outcomeFeedCode3 = outcomeFeedCode3;
-		}
-		public String getOutcomeFeedCode4() {
-			return outcomeFeedCode4;
-		}
-		public void setOutcomeFeedCode4(String outcomeFeedCode4) {
-			this.outcomeFeedCode4 = outcomeFeedCode4;
-		}
-		@Override
-		public String toString() {
-			return "HorseRacingDividend [outcomeFeedCode1=" + outcomeFeedCode1 + ", outcomeFeedCode2="
-					+ outcomeFeedCode2 + ", outcomeFeedCode3=" + outcomeFeedCode3 + ", outcomeFeedCode4="
-					+ outcomeFeedCode4 + "]";
-		}
-	}
+	
 	
 	
 	public static void main(String[] args) {
 		List<String> feedCodes;
 		HorseRacingDividend dividend;
 		MethodReflection foo = new MethodReflection();
-
+		
+		//Two outcomes work
 		dividend = new HorseRacingDividend();
 		feedCodes = Arrays.asList("BGC:123", "BGC:246");
 
 		System.out.println(dividend);
 
-		
+		//Four outcomes still work
 		dividend = new HorseRacingDividend();
 		feedCodes = Arrays.asList("BGC:123", "BGC:246", "BGC:369", "BGC:4812");
 
 		foo.setWinningFeedCodesOldSchool(dividend, feedCodes);
 		System.out.println(dividend);
 
+		
+		//FIVE OUTCOMES! Cannot handle this
+		dividend = new HorseRacingDividend();
+		feedCodes = Arrays.asList("BGC:123", "BGC:246", "BGC:369", "BGC:4812", "BGC:51015");
+
+		foo.setWinningFeedCodesOldSchool(dividend, feedCodes);
+		System.out.println(dividend);
 		
 		dividend = new HorseRacingDividend();
 		feedCodes = Arrays.asList("BGC:123", "BGC:246");
@@ -69,6 +42,13 @@ public class MethodReflection {
 
 		dividend = new HorseRacingDividend();
 		feedCodes = Arrays.asList("BGC:123", "BGC:246", "BGC:369", "BGC:4812");
+
+		foo.setWinningFeedCodesReflection(dividend, feedCodes);
+		System.out.println(dividend);
+		
+		//FIVE OUTCOMES! EZ
+		dividend = new HorseRacingDividend();
+		feedCodes = Arrays.asList("BGC:123", "BGC:246", "BGC:369", "BGC:4812", "BGC:51015");
 
 		foo.setWinningFeedCodesReflection(dividend, feedCodes);
 		System.out.println(dividend);
@@ -97,7 +77,6 @@ public class MethodReflection {
 		if(feedCodes.size() >= 4) {
 			dividend.setOutcomeFeedCode4(feedCodes.get(3));
 		}
-		
 	}
 	
 	public  void setWinningFeedCodesReflection(HorseRacingDividend dividend, List<String> feedCodes) {
@@ -112,4 +91,18 @@ public class MethodReflection {
 			}	
 		}
 	}
+	
+	/**
+	 * HERE BE DRAGONS:
+	 * 
+	 * Only use reflections when you have to.
+	 * CONS:
+	 * 1. Slower than normal method invocation
+	 * 2. Dangerous (lots of possibilities for Exceptions, what if the names change?)
+	 * 
+	 * PROS:
+	 * 1. Useful when you cannot pre-determine the appropriate method to use (eg, mapping excel fields based on column header) 
+	 * 2. Useful when you want to invoke multiple methods based on a patter (eq, in the past, for junit, method  ends with "...Test")
+	 * 
+	 */
 }
