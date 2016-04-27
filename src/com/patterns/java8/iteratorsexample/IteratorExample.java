8431d30c9e624b1646ce7f7ad30a070332653263
Jc Catalan <jccatalan.betica@gmail.com>
2016-04-27 19:00:40 +0800

    Added loops

--- src/com/patterns/java8/iteratorsexample/IteratorExample.java
+++ src/com/patterns/java8/iteratorsexample/IteratorExample.java
@@ -8,19 +8,35 @@ public class IteratorExample {
 	public static void main(String[] args){
 		List<Integer> numbers = Arrays.asList(1,2,3,5,6,4,7,8,9,10);
 		
+		for (Integer integer : numbers) {
+			System.out.println(integer);
+		}
 		
 		System.out.println("Lambda");
-		numbers.forEach(
-				(value) -> System.out.print(value));
+		numbers.forEach((value) -> System.out.println(value));
 
-		System.out.println("\n\nMethod reference");
+		System.out.println("Method reference");
 		numbers.forEach(System.out::print);
-
-		System.out.println("\n\nStream");
+		
+		System.out.println("Foreach sum");
+		int result = 0;
+		for (Integer integer : numbers) {
+			result += integer;
+		}
+		System.out.println(result);
+		
+		System.out.println("Stream sum");
 		System.out.print(numbers.stream().map(e -> e * 2 ).reduce(0, (c,e) -> c + e));
 		
-
-		System.out.println("\n\nStream");
+		System.out.println("Foreach with Filters");
+		for (Integer integer : numbers) {
+			if (integer > 3 && integer % 2 == 0) {
+				System.out.println(integer);
+				break;
+			}
+		}
+		
+		System.out.println("Stream with Filters");
 		System.out.print(numbers.stream()
 				.filter(IteratorExample::isGreaterThan3)
 				.filter(e -> e % 2 == 0)
