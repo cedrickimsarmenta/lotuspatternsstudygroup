88701b23e55622adfde1c14addb49a913bae7017
Blessie Gamboa <blessilda.gamboa@betica.com>
2017-01-18 18:58:15 +0800

    regex examples

--- src/com/regex/blessie/RegexSamples.java
+++ src/com/regex/blessie/RegexSamples.java
@@ -14,9 +14,9 @@ public class RegexSamples {
 	
 	public static void main(String[] args) throws IOException {
 		inputText = new String(Files.readAllBytes(Paths.get("resources/waldo.xml")));
-		findWaldoLine();
+		findWaldoWithGrouping();
+		findWaldoLine();	
 		lookForwardToWaldo();
-		findWaldo();
 	}
 	
 	private static void findWaldoLine() {
@@ -25,9 +25,9 @@ public class RegexSamples {
 		wheresWaldo(patternString, inputText);
 	}
 	
-	private static void findWaldo() {
+	private static void findWaldoWithGrouping() {
 		System.out.println("findWaldo");
-		String patternString = ".*(\\<waldo\\>.*\\</waldo\\>).*";
+		String patternString = "(\\<waldo\\>.*\\</waldo\\>)";
 		wheresWaldo(patternString, inputText);
 	}
 		
@@ -39,11 +39,14 @@ public class RegexSamples {
 			
 	private static Duration wheresWaldo(String patternString, String inputText){
 		Pattern pattern = Pattern.compile(patternString);
-		Matcher matcher = pattern.matcher(inputText);
+
 		Instant start = Instant.now();
 
-		while (matcher.find()) {
-            System.out.println(matcher.group());
+		for (int i=0; i<10000; i++) {
+			Matcher matcher = pattern.matcher(inputText);
+			while (matcher.find()) {
+//	            System.out.println(matcher.group());
+			}
 		}
 		
 		Instant end = Instant.now();
